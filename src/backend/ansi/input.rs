44aa94580e1ae07f4e494ce5285c099f5b7420e9
Lucien Greathouse <me@lpghatguy.com>
2018-12-06 17:01:36 -0800

    Handle Backspace differences a bit

--- src/backend/ansi/input.rs
+++ src/backend/ansi/input.rs
@@ -28,46 +28,57 @@ pub fn start_input_thread(sender: mpsc::Sender<u8>) {
     });
 }
 
+fn read_key_after_esc(receiver: &mpsc::Receiver<u8>, output: &mut VecDeque<Key>) {
+    match receiver.recv_timeout(Duration::from_millis(20)) {
+        Ok(second_byte) => {
+            if second_byte == b'[' {
+                match receiver.recv_timeout(Duration::from_millis(20)) {
+                    Ok(third_byte) => {
+                        match third_byte {
+                            b'A' => output.push_back(Key::Up),
+                            b'D' => output.push_back(Key::Left),
+                            b'B' => output.push_back(Key::Down),
+                            b'C' => output.push_back(Key::Right),
+                            b'H' => output.push_back(Key::Home),
+                            b'F' => output.push_back(Key::End),
+                            _ => {
+                                output.push_back(Key::Escape);
+                                output.push_back(Key::Char(second_byte.into()));
+                                output.push_back(Key::Char(third_byte.into()));
+                            },
+                        }
+                    },
+                    Err(RecvTimeoutError::Timeout) => {
+                        output.push_back(Key::Escape);
+                        output.push_back(Key::Char(second_byte.into()));
+                    },
+                    Err(RecvTimeoutError::Disconnected) => {},
+                }
+            } else {
+                output.push_back(Key::Escape);
+                output.push_back(Key::Char(second_byte.into()));
+            }
+        },
+        Err(RecvTimeoutError::Timeout) => {
+            output.push_back(Key::Escape);
+        },
+        Err(RecvTimeoutError::Disconnected) => {},
+    }
+}
+
 pub fn read_key(receiver: &mpsc::Receiver<u8>, output: &mut VecDeque<Key>) {
     let byte = receiver.recv().unwrap();
 
-    if byte == ESC {
-        match receiver.recv_timeout(Duration::from_millis(20)) {
-            Ok(second_byte) => {
-                if second_byte == b'[' {
-                    match receiver.recv_timeout(Duration::from_millis(20)) {
-                        Ok(third_byte) => {
-                            match third_byte {
-                                b'A' => output.push_back(Key::Up),
-                                b'D' => output.push_back(Key::Left),
-                                b'B' => output.push_back(Key::Down),
-                                b'C' => output.push_back(Key::Right),
-                                b'H' => output.push_back(Key::Home),
-                                b'F' => output.push_back(Key::End),
-                                _ => {
-                                    output.push_back(Key::Escape);
-                                    output.push_back(Key::Char(second_byte.into()));
-                                    output.push_back(Key::Char(third_byte.into()));
-                                },
-                            }
-                        },
-                        Err(RecvTimeoutError::Timeout) => {
-                            output.push_back(Key::Escape);
-                            output.push_back(Key::Char(second_byte.into()));
-                        },
-                        Err(RecvTimeoutError::Disconnected) => {},
-                    }
-                } else {
-                    output.push_back(Key::Escape);
-                    output.push_back(Key::Char(second_byte.into()));
-                }
-            },
-            Err(RecvTimeoutError::Timeout) => {
-                output.push_back(Key::Escape);
-            },
-            Err(RecvTimeoutError::Disconnected) => {},
-        }
-    } else {
-        output.push_back(Key::Char(byte.into()));
+    match byte {
+        ESC => {
+            read_key_after_esc(receiver, output);
+        },
+        0x7f | 0x08 => {
+            // 0x7f is for Windows, this might not be correct elsewhere?
+            output.push_back(Key::Backspace);
+        },
+        _ => {
+            output.push_back(Key::Char(byte.into()));
+        },
     }
 }
